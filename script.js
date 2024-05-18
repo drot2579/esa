3ef23d85548f59a6e69c446727b58149b8ee058e
drot2579 <154900509+drotCode@users.noreply.github.com>
2024-05-18 19:11:23 +0300

    ...

--- script.js
+++ script.js
@@ -4,24 +4,19 @@ const toggleHidden = (els, act = 0) => {
 }
 const playBlur = (els) => {
     els.forEach((el) => {
-        el.animate([
-            { filter: " brightness(1.3) drop-shadow(0 0 6px #fff)", textShadow: "0 0 2px #fff" },
-            { filter: " brightness(1)" },
-        ],
+        el.animate([{ filter: " brightness(1.3) drop-shadow(0 0 6px #fff)", textShadow: "0 0 4px #fff" }, {},],
             { duration: 250, })
-    }
-    )
+    })
 }
 const limitDecimals = (num, dcm = 1) => (Number.isInteger(+num) ? +num : +num.toFixed(dcm))
 
 /* ---------- ---------- ---------- -------- ---------- ---------- ---------- */
-const paramInstances = []
-const inpKeys = ["number", "range", "date",]
 class Param {
     postFns = [];
     callPostFns() { this.postFns.forEach((fn) => fn()) }
-    constructor(name, value = 0, attributes = {}) {
+    constructor(name, value = 0, attributes = {},decimalLimit = 1) {
         this.name = name;
+        this.decimalLimit = decimalLimit;
 
         this.inputs = Array.from(document.querySelectorAll(`input.${name}`))
         this.outputs = Array.from(document.querySelectorAll(`input:not([type=date]).${name}, .ptext.${name}`))
@@ -31,44 +26,42 @@ class Param {
             for (const attr in attributes) { input.setAttribute(attr, attributes[attr]) }
         }
         this.setValue(value)
-        paramInstances.push(this)
     }
     setValue(x) {
         if (this.value == x) { return }
         if (this.name.toLowerCase().includes("date")) { x = x.split("-").reverse().join(".") }
         this.value = x = Number.isNaN(x - 0) ? x : (x - 0)
-        if (typeof x == "number") { x = limitDecimals(x) }
+        if (typeof x == "number") { x = limitDecimals(x,this.decimalLimit) }
         for (const el of this.outputs) { el.value = el.innerText = x; playBlur([el]) }
         this.callPostFns()
     }
 }
 
-
+const [ertMax, ertMin, drbMax, drbMin, kgMax, kgMin, hgbCutoff] = [150, 75, 0.75, 0.35, 150, 0, 11];
 let hgb = new Param("hgb", 10, { min: 5, max: 12, step: 0.1 })
-let kg = new Param("kg", 50, { min: 0, max: 150, step: 0.5 })
-let ert = new Param("ert", 7500, { min: 0, max: 150 * 150, step: 75 })
-let ertPk = new Param("ertPk", 150, { min: 0, max: 150, step: 75 })
-let drb = new Param("drb", 37.5, { min: 0, max: 0.75 * 150, step: 0.35 })
-let drbPk = new Param("drbPk", 0.75, { min: 0, max: 0.75, step: 0.05 })
+let kg = new Param("kg", 50, { min: 0, max: kgMax, step: 0.5 })
+let ert = new Param("ert", 7500, { min: 0, max: ertMax * kgMax, step: ertMin })
+let ertPk = new Param("ertPk", 150, { min: 0, max: ertMax, step: ertMin })
+let drb = new Param("drb", 37.5, { min: 0, max: drbMax * kgMax, step: drbMin })
+let drbPk = new Param("drbPk", 0.75, { min: 0, max: drbMax, step: drbMin },2)
 let fer = new Param("fer", 300, { min: 201, max: 1500, step: 1 })
 
 let day = 1000 * 60 * 60 * 24
 let date = new Date()
 let currentDate = date.toISOString().slice(0, 10)
-let firstDayOfMonth = date.toISOString().slice(0, 8) + "01"
+let firstOfMonth = date.toISOString().slice(0, 8) + "01"
 let sixMonthsAgo = new Date(date - 180 * day).toISOString().slice(0, 10)
 
-let hgbDate = new Param("hgbDate", firstDayOfMonth, { min: sixMonthsAgo, max: currentDate })
-let ferDate = new Param("ferDate", firstDayOfMonth, { min: sixMonthsAgo, max: currentDate })
-
+let hgbDate = new Param("hgbDate", firstOfMonth, { value: firstOfMonth, min: sixMonthsAgo, max: currentDate })
+let ferDate = new Param("ferDate", firstOfMonth, { value: firstOfMonth, min: sixMonthsAgo, max: currentDate })
 
 const updateDoses = () => {
     ert.setValue(ertPk.value * kg.value)
     drb.setValue(drbPk.value * kg.value)
 }
 const hgbToPk = () => {
-    ertPk.setValue(hgb.value < 11 ? 150 : 75)
-    drbPk.setValue(hgb.value < 11 ? 0.75 : 0.35)
+    ertPk.setValue(hgb.value < hgbCutoff ? ertMax : ertMin)
+    drbPk.setValue(hgb.value < hgbCutoff ? drbMax : drbMin)
     updateDoses()
 }
 hgb.postFns.push(hgbToPk)
@@ -78,13 +71,11 @@ drb.postFns.push(() => kg.setValue(drb.value / drbPk.value))
 console.timeEnd("a")
 
 
-const rxxSwc = document.querySelector("input[type=checkbox].rxx")
+const rxxSwitch = document.querySelector("input[type=checkbox].rxx")
 toggleHidden(document.querySelectorAll(".rxx"))
-rxxSwc.addEventListener("change", (e) => {
+rxxSwitch.addEventListener("change", (e) => {
     toggleHidden(document.querySelectorAll(".rxx"))
 })
-
-
 
 
 const ertSwitchEl = document.querySelector("input[type=checkbox].ertVisible")
@@ -103,18 +94,18 @@ drbSwitchEl.addEventListener("change", ertdrbFn);
 let popTimeout = null;
 
 
-let pop = document.querySelector(".pop") 
- const popHandler = () => {
-pop.classList.remove("passive")
-popTimeout =  setTimeout(() => pop.classList.add("passive"), 1000 )
- }
+let pop = document.querySelector(".pop")
+const popHandler = () => {
+    pop.classList.remove("nopop")
+    popTimeout = setTimeout(() => pop.classList.add("nopop"), 1000)
+}
 
 for (const el of document.querySelectorAll(".sentence")) {
-    el.addEventListener("dblclick", (e) => {
+    el.addEventListener("click", (e) => {
         let text = el.textContent.trim()
-        text =  text.replace(/[\ ,\n]+/g, " ")
+        text = text.replace(/[\ ,\n]+/g, " ")
         console.log(text);
         navigator.clipboard.writeText(text)
         popHandler()
-    },{capture:true})
+    })
 }
